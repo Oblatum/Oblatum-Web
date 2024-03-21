be0b1c539335e05025ff1c6bb7380976e2037a2e
PzHown <1263097055@qq.com>
2024-03-21 17:41:48 +0800

    添加 github raw 代理均衡

--- src/stores/github.ts
+++ src/stores/github.ts
@@ -2,14 +2,19 @@ import { defineStore } from 'pinia'
 
 export const useGithubStore = defineStore('github', {
     state: () => ({
-        //proxy:"https://gh.api.99988866.xyz/",
-        proxy:"https://hub.gitmirror.com/",
+        proxy:["https://gh.api.99988866.xyz/","https://hub.gitmirror.com/"],
+        proxy2:"https://cdn.jsdelivr.net/gh/",
         url:"https://raw.githubusercontent.com/Oblatum/Oblatum-IconPack-Reborn/master/"
     }),
     getters: {
         getGithubUrl():string{
             // return this.url.replace("raw.githubusercontent.com","raw.staticdn.net");
-            return this.url;
+            //66%的概率使用proxy，33%的概率使用proxy2
+            const random = Math.random();
+            if(random<0.66){
+                return this.proxy[Math.floor(Math.random()*this.proxy.length)]+this.url;
+            }
+            return this.proxy2+this.url.split("com/")[1].split("/master")[0]+"@master/";
         }
     }
 
